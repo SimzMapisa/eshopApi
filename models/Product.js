16e24efc4eae5f0d79b0d0b614a8e2b9d7f215ff
SimzMapisa <simbarashe795@gmail.com>
2022-01-25 02:13:30 +0200

    modified the product model export statement and field types

--- models/Product.js
+++ models/Product.js
@@ -1,23 +1,22 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const productSchema = new Schema({
+// Create a schema 
+const ProductSchema = new Schema({
     name: {
-        type: 'String',
+        type: String,
         required: true,
     },
     description: {
-        type: 'String',
+        type: String,
         required: true,
     },
 
     qtyInStock: {
-        type: 'Number',
+        type: Number,
         required: true,
     },
     date: { type: Date, default: Date.now },
 })
 
-const product = mongoose.model('Product', productSchema);
-
-exports.Product;
+module.exports = Product = mongoose.model('products', ProductSchema);
