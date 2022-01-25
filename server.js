35ec8432609c866942c1d3dfb823a48d78d4b990
SimzMapisa <simbarashe795@gmail.com>
2022-01-25 15:48:23 +0200

    added user routes

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const connectDb = require('./dbConfig');
 const app = express();
 
 const products = require('./routes/products')
+const users = require('./routes/customers')
 
 
 
@@ -11,12 +12,14 @@ require('dotenv').config();
 
 
 // Middleware
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 app.use(morgan('tiny'));
 
 connectDb();
 
 app.use('/api/products', products)
+app.use('/api/users', users)
 
 app.listen(3000, () => {
     console.log('server running on port 3000')
