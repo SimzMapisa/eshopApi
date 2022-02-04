181dd46e36c9c907d89b24f2984c72194bdc31d6
SimzMapisa <simbarashe795@gmail.com>
2022-02-04 21:00:35 +0200

    created product mdoel

--- models/Product.js
+++ models/Product.js
@@ -11,7 +11,28 @@ const ProductSchema = new Schema({
         type: String,
         required: true,
     },
-
+    richDescription: {
+        type: String,
+        default: ''
+    },
+    img: String,
+    images: [{
+        type: String,
+    }],
+    brand: {
+        type: String,
+        default: ''
+    },
+    price: {
+        type: Number,
+        default: 0,
+        required: true
+    },
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category',
+        required: true
+    },
     qtyInStock: {
         type: Number,
         required: true,
