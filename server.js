329541216bdc6b9571764c7f99866413a595db84
SimzMapisa <simbarashe795@gmail.com>
2022-02-04 20:56:52 +0200

    added cors, some more routes and passport js

--- server.js
+++ server.js
@@ -3,16 +3,18 @@ const morgan = require('morgan');
 const connectDb = require('./dbConfig');
 const app = express();
 const passport = require('passport');
+const cors = require('cors')
 
 const products = require('./routes/products')
-const users = require('./routes/customers')
-
-
+const users = require('./routes/users')
+const categories = require('./routes/categories')
 
 require('dotenv').config();
 
 
 // Middleware
+app.use(cors())
+app.options('*', cors)
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 app.use(morgan('tiny'));
@@ -25,6 +27,7 @@ connectDb();
 
 app.use('/api/products', products)
 app.use('/api/users', users)
+app.use('/api/categories', categories)
 
 app.listen(3000, () => {
     console.log('server running on port 3000')
