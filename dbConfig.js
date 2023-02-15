7198dea7f23bf7ed14fc344e849418c555347b0f
SimzMapisa <simbarashe795@gmail.com>
2023-02-15 20:27:59 +0000

    changed connect uri ip address

--- dbConfig.js
+++ dbConfig.js
@@ -1,15 +1,16 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const connectDb = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_LOCAL_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
-        console.log('connected succesfully')
-    } catch (error) {
-        console.log(error.message)
-    }
-}
+	try {
+		await mongoose.connect(process.env.MONGO_LOCAL_URI, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		});
+		console.log('connected succesfully');
+	} catch (error) {
+		console.log(error.message);
+	}
+};
 
 module.exports = connectDb;
