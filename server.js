cd252aa898f8fcba0622fa12412a39e53a76a30d
SimzMapisa <simbarashe795@gmail.com>
2023-02-15 20:26:48 +0000

    clean up the file removing unwanted imports and put them in their respective files

--- server.js
+++ server.js
@@ -3,20 +3,18 @@ const morgan = require('morgan');
 const connectDb = require('./dbConfig');
 const app = express();
 const passport = require('passport');
-const cors = require('cors')
+const cors = require('cors');
 
-const products = require('./routes/products')
-const users = require('./routes/users')
-const categories = require('./routes/categories')
-
-require('dotenv').config();
-
+const products = require('./v1/routes/products');
+const users = require('./v1/routes/users');
+const categories = require('./v1/routes/categories');
+const createAccount = require('./v1/routes/users');
 
 // Middleware
-app.use(cors())
-app.options('*', cors)
+app.use(cors());
+app.options('*', cors);
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json())
+app.use(express.json());
 app.use(morgan('tiny'));
 app.use(passport.initialize());
 
@@ -25,10 +23,10 @@ require('./config/passport')(passport);
 
 connectDb();
 
-app.use('/api/products', products)
-app.use('/api/users', users)
-app.use('/api/categories', categories)
+app.use('/api/v1/products', products);
+app.use('/api/v1/users', users);
+app.use('/api/v1/categories', categories);
 
 app.listen(3000, () => {
-    console.log('server running on port 3000')
-})
+	console.log('server running on port 3000');
+});
