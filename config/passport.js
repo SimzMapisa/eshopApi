2124d511d01b8258e70300c80faae1cc37d2751a
SimzMapisa <simbarashe795@gmail.com>
2023-02-16 22:02:55 +0000

    fix extractJwt import statement

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,5 @@
 const JwtStrategy = require('passport-jwt').Strategy;
-ExtractJwt = require('passport-jwt').ExtractJwt;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
 const mongoose = require('mongoose');
 const User = mongoose.model('users');
 const Secret = process.env.secretOrKey;
@@ -8,19 +8,20 @@ const options = {};
 options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 options.secretOrKey = Secret;
 
-module.exports = passport => {
-    passport.use(new JwtStrategy(options, (jwt_payload, done) => {
-        // console.log(jwt_payload);
-        User.findById(jwt_payload.id)
-            .then(user => {
-                if (user) {
-                    return done(null, user)
-                }
-                return done(null, false)
-            })
-            .catch(err => {
-                console.log(err);
-            })
-    })
-    );
-}
+module.exports = (passport) => {
+	passport.use(
+		new JwtStrategy(options, (jwt_payload, done) => {
+			// console.log(jwt_payload);
+			User.findById(jwt_payload.id)
+				.then((user) => {
+					if (user) {
+						return done(null, user);
+					}
+					return done(null, false);
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		})
+	);
+};
