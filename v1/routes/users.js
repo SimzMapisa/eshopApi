c1911915835743d5ace6a076de462fa21e34a971
SimzMapisa <simbarashe795@gmail.com>
2023-02-16 20:33:56 +0000

    move the login to controllers then create a route to get a single user

--- v1/routes/users.js
+++ v1/routes/users.js
@@ -1,11 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User');
-const gravatar = require('gravatar');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const passport = require('passport');
-const { body, validationResult } = require('express-validator');
+const validator = require('../../validators');
 const UserController = require('../controllers/users');
 
 // @route   POST /register
@@ -13,68 +8,8 @@ const UserController = require('../controllers/users');
 // @access  Public
 router.post(
 	'/register',
-	body('email').isEmail().normalizeEmail(),
-	body('name').isLength({ min: 2 }),
-	body('surname').isLength({ min: 2 }),
-	body('password')
-		.isLength({ min: 5 })
-		.isLength({ min: 5 })
-		.withMessage('password must be at least 5 characters')
-		.matches('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$')
-		.withMessage(
-			'Minimum eight characters, at least one upper case English letter, one lower case English letter, one number and one special character'
-		),
-	body('confirmPassword').custom((value, { req }) => {
-		if (value !== req.body.password) {
-			throw new Error('Password confirmation does not match password');
-		}
-		// Indicates the success of this synchronous custom validator
-		return true;
-	}),
-
-	(req, res) => {
-		// Check is there any errors
-		const errors = validationResult(req);
-		if (!errors.isEmpty()) {
-			return res.status(400).json({ errors: errors.array() });
-		}
-
-		// Check if the user already exist
-		User.findOne({ email: req.body.email }).then((user) => {
-			if (user) {
-				return res
-					.status(400)
-					.json({ email: 'account already exist please login instead!' });
-			} else {
-				// If the user doesnt already exist get their gravatar and store in a variable
-				const avatar = gravatar.url(req.body.email, {
-					s: '200', //size
-					r: 'pg', //raitng
-					d: 'mm', // default
-				});
-
-				// Use the User model to create a new instance of the User
-				const newUser = new User({
-					name: req.body.name,
-					surname: req.body.surname,
-					email: req.body.email,
-					avatar,
-					password: req.body.password,
-				});
-
-				bcrypt.genSalt(10, (err, salt) => {
-					bcrypt.hash(newUser.password, salt, (err, hash) => {
-						if (err) throw err;
-						newUser.password = hash;
-						newUser
-							.save()
-							.then((user) => res.json(user))
-							.catch((error) => console.log(error));
-					});
-				});
-			}
-		});
-	}
+	validator.validateUserCreation,
+	UserController.RegisterUser
 );
 
 // @route   GET /users/login
@@ -82,10 +17,14 @@ router.post(
 // @access  Public
 router.post('/login', UserController.UserLogin);
 
-// @route   GET /api/users/current
+// @route   GET /api/v1/users
+// @desc    Returns  all users
+// @access  Private
+router.get('/', UserController.getAllUsers);
+
+// @route   GET /api/v1/users/:id
 // @desc    Returns  the current user
 // @access  Private
-
-router.get('/', UserController.getAllUsers);
+router.get('/:id', UserController.getSingleUser);
 
 module.exports = router;
