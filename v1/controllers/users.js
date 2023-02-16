9f7bd062a0f794f01d699db118bbade6163f7ef0
SimzMapisa <simbarashe795@gmail.com>
2023-02-16 20:35:28 +0000

    add logic to get a single user

--- v1/controllers/users.js
+++ v1/controllers/users.js
@@ -1,6 +1,54 @@
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const gravatar = require('gravatar');
+const { validationResult } = require('express-validator');
+
+const RegisterUser = (req, res) => {
+	// Check is there any errors
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		return res.status(400).json({ errors: errors.array() });
+	}
+
+	// Check if the user already exist
+	User.findOne({ email: req.body.email }).then((user) => {
+		if (user) {
+			return res
+				.status(400)
+				.json({ email: 'account already exist please login instead!' });
+		} else {
+			// If the user doesnt already exist get their gravatar and store in a variable
+			const avatar = gravatar.url(req.body.email, {
+				s: '200', //size
+				r: 'pg', //raitng
+				d: 'mm', // default
+			});
+
+			// Use the User model to create a new instance of the User
+			const newUser = new User({
+				name: req.body.name,
+				surname: req.body.surname,
+				email: req.body.email,
+				avatar,
+				password: req.body.password,
+			});
+
+			bcrypt.genSalt(10, (err, salt) => {
+				bcrypt.hash(newUser.password, salt, (err, hash) => {
+					if (err) throw err;
+					newUser.password = hash;
+					newUser
+						.save()
+						.then((user) => res.json(user))
+						.catch((error) => console.log(error));
+				});
+			});
+		}
+	});
+};
+
+// Login into an existing account
 
 const UserLogin = (req, res) => {
 	const email = req.body.email;
@@ -35,10 +83,10 @@ const UserLogin = (req, res) => {
 	});
 };
 
+// Get a list of all the available users
 const getAllUsers = (req, res) => {
 	User.find()
 		.then((users) => {
-			console.log(typeof users);
 			res.json(users);
 		})
 		.catch((err) => {
@@ -46,7 +94,16 @@ const getAllUsers = (req, res) => {
 		});
 };
 
+const getSingleUser = (req, res) => {
+	User.findById(req.params.id).then((user) => {
+		console.log(user);
+		res.json(user);
+	});
+};
+
 module.exports = {
 	getAllUsers,
 	UserLogin,
+	RegisterUser,
+	getSingleUser,
 };
