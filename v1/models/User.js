085507020e12047132a1e09305e6d335ae590b52
SimzMapisa <simbarashe795@gmail.com>
2023-02-16 20:31:22 +0000

    add json logic to remove password when returning users

--- v1/models/User.js
+++ v1/models/User.js
@@ -1,34 +1,43 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
-	name: {
-		type: String,
-		required: true,
-	},
-	surname: {
-		type: String,
-		required: true,
-	},
-	email: {
-		type: String,
-		required: true,
-	},
+const UserSchema = new Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		surname: {
+			type: String,
+			required: true,
+		},
+		email: {
+			type: String,
+			required: true,
+		},
 
-	password: {
-		type: String,
-		required: true,
-	},
+		password: {
+			type: String,
+			required: true,
+		},
 
-	avatar: {
-		type: String,
-	},
+		avatar: {
+			type: String,
+		},
 
-	date: {
-		type: Date,
-		default: Date.now,
+		date: {
+			type: Date,
+			default: Date.now,
+		},
 	},
-});
+	{
+		toJSON: {
+			transform(doc, ret) {
+				delete ret.password;
+			},
+		},
+	}
+);
 
 let User;
 
