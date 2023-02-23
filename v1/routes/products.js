af48d7b69f1564ebe0be45aa2cd0be916acab72c
SimzMapisa <simbarashe795@gmail.com>
2023-02-23 22:35:26 +0000

    add update product route only accessed by ADMIN and SUPER_ADMIN

--- v1/routes/products.js
+++ v1/routes/products.js
@@ -1,23 +1,34 @@
 const express = require('express');
 const router = express.Router();
-const ProductRoutes = require('../controllers/products.controller');
+const {
+	GetAllProducts,
+	GetProduct,
+	CreateProduct,
+	EditProduct,
+} = require('../controllers/products.controller');
+const { userAuth } = require('../middleware/auth');
 
 // @route   Get api/products
 // @desc - Get all products
 // @access - Public
 
-router.get('/', ProductRoutes.GetAllProducts);
+router.get('/', GetAllProducts);
 
 // @route   Get api/products
 // @desc - Get all products
 // @access - Public
 
-router.get('/:id', ProductRoutes.GetProduct);
+router.get('/:id', GetProduct);
 
 // @route   POST api/products
 // @desc -  Post a single product
-// @access - Public
+// @access - Private (only ADMINS and SUPER_ADMINS can access)
 
-router.post('/', ProductRoutes.CreateProduct);
+router.post('/', userAuth(['SUPER_ADMIN', 'ADMIN']), CreateProduct);
+
+// @route   POST api/products
+// @desc -  Post a single product
+// @access - Private (only ADMINS and SUPER_ADMINS can access)
+router.patch('/:id', userAuth(['SUPER_ADMIN', 'ADMIN']), EditProduct);
 
 module.exports = router;
