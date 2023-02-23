07fd16885cdc1e673839f7cdaf222a0e0a49c822
SimzMapisa <simbarashe795@gmail.com>
2023-02-23 22:30:56 +0000

    add userAuth function to authenticate users based on role
    The function accepts role parameter which is initially an empty array
    when calling the function in routes it should have an array
    argument of roles

--- v1/middleware/auth.js
+++ v1/middleware/auth.js
@@ -1,18 +1,28 @@
 const jwt = require('jsonwebtoken');
-
-const userAuth = (req, res, next) => {
-	try {
-		const token = req.headers.authorization.split(' ')[1];
-		console.log(token);
-		const decoded = jwt.verify(token, process.env.secretOrKey);
-		req.userData = decoded;
-		next();
-	} catch (error) {
-		return res
-			.status(401)
-			.json({ message: 'auth failed, Please login to access these resources' });
-	}
-};
+/**
+ *
+ * @param roles is an array of one or more roles
+ * @returns authenticates based on given role if the role is not found in the decoded token then it throws an error
+ */
+const userAuth =
+	(roles = []) =>
+	(req, res, next) => {
+		try {
+			const token = req.headers.authorization.split(' ')[1];
+			const decoded = jwt.verify(token, process.env.secretOrKey);
+			if (!roles.includes(decoded.role)) {
+				throw new Error();
+			}
+			req.userData = decoded;
+			next();
+		} catch (error) {
+			return res.status(401).json({
+				message:
+					'Unauthorized access, you are not allowed to access these resources',
+				success: false,
+			});
+		}
+	};
 
 module.exports = {
 	userAuth,
