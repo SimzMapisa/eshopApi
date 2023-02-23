07c4f3bbfd6f11373593dd6529dc98301596e6b2
SimzMapisa <simbarashe795@gmail.com>
2023-02-23 22:38:49 +0000

    add  update product functionality

--- v1/controllers/products.controller.js
+++ v1/controllers/products.controller.js
@@ -42,8 +42,36 @@ const CreateProduct = (req, res) => {
 	});
 };
 
+const EditProduct = async (req, res) => {
+	try {
+		await Product.findById(req.params.id).then((product) => {
+			if (!product) {
+				res.status(404).json({ message: 'Product not found' });
+			}
+
+			product.name = req.body.name || product.name;
+			product.slug = req.body.name || product.name;
+			product.description = req.body.description || product.description;
+			product.qtyInStock = req.body.qtyInStock || product.qtyInStock;
+			product.richDescription =
+				req.body.richDescription || product.richDescription;
+			product.img = req.body.img || product.img;
+			product.price = req.body.price || product.price;
+			product.category = req.body.category || product.category;
+			product.brand = req.body.brand || product.brand;
+
+			product.save();
+			res.status(200).json({ message: 'Product saved successfully' });
+		});
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ message: 'Error updating product' });
+	}
+};
+
 module.exports = {
 	CreateProduct,
 	GetAllProducts,
 	GetProduct,
+	EditProduct,
 };
