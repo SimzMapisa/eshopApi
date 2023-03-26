c30eed1d57eb5a2382b562d12f4880e69d96f604
SimzMapisa <simbarashe795@gmail.com>
2023-03-26 23:21:32 +0100

    add functionality for produuct controller

--- v1/controllers/products.controller.js
+++ v1/controllers/products.controller.js
@@ -6,42 +6,46 @@ const GetAllProducts = (req, res) => {
 		.catch((err) => console.log(err));
 };
 
-const GetProduct = (req, res) => {
-	Product.findById(req.params.id).then((product) => {
-		if (!product)
-			return res.status(404).json({ message: 'Product can not be found!' });
-		console.log(product);
-		res.json(product);
-	});
+const GetProduct = async (req, res) => {
+	await Product.findById(req.params.id)
+		.exec()
+		.then((product) => {
+			if (!product)
+				return res.status(404).json({ message: 'Product can not be found!' });
+			console.log(product);
+			res.json(product);
+		});
 };
 
-const CreateProduct = (req, res) => {
-	Product.findOne({ name: req.body.name }).then((product) => {
-		if (product && product.qtyInStock !== 0) {
-			return res.status(400).json({ product: 'product already exists' });
-		} else {
-			const newProduct = new Product({
-				name: req.body.name,
-				slug: req.body.name,
-				description: req.body.description,
-				qtyInStock: req.body.qtyInStock,
-				richDescription: req.body.richDescription,
-				img: req.body.img,
-				price: req.body.price,
-				category: req.body.category,
-				brand: req.body.brand,
-			});
-
-			newProduct
-				.save()
-				.then((product) => {
-					res.json(product);
-				})
-				.catch((error) => {
-					console.log(error.message);
+const CreateProduct = async (req, res) => {
+	await Product.findOne({ name: req.body.name })
+		.exec()
+		.then((product) => {
+			if (product && product.qtyInStock !== 0) {
+				return res.status(400).json({ product: 'product already exists' });
+			} else {
+				const newProduct = new Product({
+					name: req.body.name,
+					slug: req.body.name,
+					description: req.body.description,
+					qtyInStock: req.body.qtyInStock,
+					richDescription: req.body.richDescription,
+					img: req.body.img,
+					price: req.body.price,
+					category: req.body.category,
+					brand: req.body.brand,
 				});
-		}
-	});
+
+				newProduct
+					.save()
+					.then((product) => {
+						res.json(product);
+					})
+					.catch((error) => {
+						console.log(error.message);
+					});
+			}
+		});
 };
 
 const EditProduct = async (req, res) => {
